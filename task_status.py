4d7861597fbb6b0d91d7b4c68262f6131b7940e2
Калинчук Павел Анатольевич <p.kalinchuk@evropolis.ru>
2022-01-11 11:03:30 +0300

    Доработан обработчик except

--- task_status.py
+++ task_status.py
@@ -9,7 +9,7 @@ import getpass
 # import time
 # import json
 
-driver_path = r'C:\Users\p.kalinchuk\python\drivers\geckodriver.exe'
+driver_path = r'C:\Users\p.kalinchuk\python\work\drivers\geckodriver.exe'
 
 options = Options()
 options.headless = True
@@ -17,11 +17,12 @@ path = Service(driver_path)
 wd = webdriver.Firefox(service=path, options=options)
 wd.implicitly_wait(30)
 
+# Читаем файл со списком номеров задач
 with open('tasks.txt', 'r') as f:
     tasks = f.read()
 
 tasks_list = tasks.split()
-
+# Получаем пароль к Jire от пользователя
 pass_key = getpass.getpass('\nПароль пользователя Jira: ')
 
 try:
@@ -42,36 +43,45 @@ try:
         By.XPATH, "//input[@id='login-form-submit']").click()
     print("-"*10 + "\n")
     for i in tasks_list:
-        print(str(i))
-        wd.get("https://jira.absolutins.ru/browse/"+str(i))
-        status_task = "   Статус: " + \
-            wd.find_element(By.XPATH, "//span[@id='status-val']").text
-        executor_task = "   Исполнитель: " + \
-            wd.find_element(
-                By.XPATH, "//span[@id='assignee-val']").text
-        author_task = "   Автор: " + \
-            wd.find_element(
-                By.XPATH, "//span[@id='reporter-val']").text
-        tester_task = "   Тестировщик: " + \
-            wd.find_element(
-                By.XPATH, "//span[@id='customfield_11200-val']").text
-        # customfield_11200-val
-        # with open('result.txt', 'a', encoding="utf-8") as f:
-        # f.write(str(i) + "\n" + status_task + "\n" +
-        # executor_task + "\n" + author_task + "\n")
-        print(status_task + "\n" + executor_task + "\n" +
-              author_task + "\n" + tester_task + "\n")
+        try:
+            print(str(i))
+            wd.get("https://jira.absolutins.ru/browse/"+str(i))
+            status_task = "   Статус: " + \
+                wd.find_element(By.XPATH, "//span[@id='status-val']").text
+            executor_task = "   Исполнитель: " + \
+                wd.find_element(
+                    By.XPATH, "//span[@id='assignee-val']").text
+            author_task = "   Автор: " + \
+                wd.find_element(
+                    By.XPATH, "//span[@id='reporter-val']").text
+            tester_task = "   Тестировщик: " + \
+                wd.find_element(
+                    By.XPATH, "//span[@id='customfield_11200-val']").text
+            # Разблокировать, если надо записывать в файл
+            # with open('result.txt', 'a', encoding="utf-8") as f:
+            # f.write(str(i) + "\n" + status_task + "\n" +
+            # executor_task + "\n" + author_task + "\n")
+            print(status_task + "\n" + executor_task + "\n" +
+                  author_task + "\n" + tester_task + "\n")
+        except NoSuchElementException as e:
+            s = str(e)
+            if "status-val" in s:
+                status_task = '    Статус: -----'
+                #print('    Не найден элемент "Статус"')
+            elif "assignee-val" in s:
+                executor_task = '    Исполнитель: -----'
+                #print('    Не найден элемент "Исполнитель"')
+            elif "reporter-val" in s:
+                author_task = '    Автор: -----'
+                #print('    Не найден элемент "Автор"')
+            elif "customfield_11200-val" in s:
+                tester_task = '   Тестировщик: -----'
+            else:
+                print('Ни один из запрошенных элементов не найден !!!')
+            print(status_task + "\n" + executor_task + "\n" +
+                  author_task + "\n" + tester_task + "\n")
     print("-"*10)
-except NoSuchElementException as e:
-    s = str(e)
-    if "status-val" in s:
-        print('    Не найден элемент "Статус"')
-    elif "assignee-val" in s:
-        print('    Не найден элемент "Исполнитель"')
-    elif "reporter-val" in s:
-        print('    Не найден элемент "Автор"')
-    elif "customfield_11200-val" in s:
-        print('    Не найден элемент "Тестировщик"')
-    print("\n" + "-"*10)
+except:
+    print("Error")
 finally:
     wd.quit()
