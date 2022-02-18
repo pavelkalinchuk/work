aafe6cbcc4415fd16353f6ef65592533f2eda131
Калинчук Павел Анатольевич <p.kalinchuk@evropolis.ru>
2022-02-18 15:22:36 +0300

    Финальная версия скрипта получения списка задач по фильтру В2В Моторы

--- take_task_list_b2b.py
+++ take_task_list_b2b.py
@@ -14,12 +14,13 @@ path = Service(driver_path)
 wd = webdriver.Firefox(service=path, options=options)
 wd.implicitly_wait(30)
 
+# Создаём пустой список
 take_list = []
 
-# Получаем пароль к Jire от пользователя
+# Получаем пароль к Jire от пользователя при этом скрываем вводимое
 pass_key = getpass.getpass('\nПароль пользователя Jira: ')
-#pass_key = input("\nПароль пользователя Jira: ")
 
+# Авторизовываемся в Jira
 try:
     wd.get("https://jira.absolutins.ru/login.jsp?os_destination=%2Flogin.jsp")
     wd.find_element(
@@ -37,24 +38,27 @@ try:
     wd.find_element(
         By.XPATH, "//input[@id='login-form-submit']").click()
     print("-"*10 + "\n")
+ # Переходим на страницу фильтра со списком задач  В2В Моторы готовые к тестированию
     wd.get("https://jira.absolutins.ru/issues/?filter=12451")
+# Собираем номера задач со страницы фильтра
     try:
         a = wd.find_elements(
             By.XPATH, '//td[@class="issuekey"]')
-#        a = wd.find_element(
-#            By.XPATH, '//td[@class="issuekey"]').text
-#        print(a)
-#        a = wd.find_elements(
-#            By.XPATH, "//*[contains(@id, 'issuerow')]")
+# Записываем полученные номера задач в список take_list
         for i in a:
             take_list.append(i.text)
-#        print(take_list)
+# Разбираем созданный список задач для более удобного восприятия
         take_list = map(lambda x: x + '\n', take_list)
+# Записываем разобранный список задач в файл
         with open('tasks.txt', 'w') as f:
             f.writelines(take_list)
+        print('Создан файл \'task.txt\' со списком задач к тестированию\n')
+# Обработка исключений (ошибок при выполнении)
     except:
         b = 'test'
+# Обработка исключений (ошибок при выполнении)
 except:
     print("Что-то пошло не так :-(\n")
+# Завершаем работу скрипта
 finally:
     wd.quit()
