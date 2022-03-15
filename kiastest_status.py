db2efe93042f6b96757d9a4e37f1e592997924e4
pavel.kalinchuk <715m4s@gmail.com>
2022-03-15 09:48:52 +0300

    исправлен путь до chromdriver

--- kiastest_status.py
+++ kiastest_status.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
 import datetime
 
-driver_path = r'drivers/chromedriver.exe'
+driver_path = r'C:\Users\p.kalinchuk\python\drivers\chromedriver.exe'
 
 #  4 строки для выполнения "без запуска" браузера
 ua = dict(DesiredCapabilities.CHROME)
